96aeaf32962297f05bfa88f336b23425f3035e40
バシュロアレキサンダーレイ <40430040+NfoAlex@users.noreply.github.com>
2023-04-24 15:29:40 +0900

    [add] - 起動に必要なディレクトリの確認項目を追加

--- index.js
+++ index.js
@@ -32,6 +32,7 @@ let sessionOnline = {};
 //必要なディレクトリの確認、なければ作成
 try{fs.mkdirSync("./fileidIndex/");}catch(e){}
 try{fs.mkdirSync("./files/");}catch(e){}
+try{fs.mkdirSync("./record/");}catch(e){}
 try{fs.mkdirSync("./img/");}catch(e){}
 
 //もしバックエンドに直接アクセスされたら用
