c168ff6a42b303f27c4eb0374c536eedec19687a
バシュロアレキサンダーレイ <40430040+NfoAlex@users.noreply.github.com>
2023-04-25 10:33:35 +0900

    [fix] - ダウンロードするときのファイル名がおかしかった

--- index.js
+++ index.js
@@ -85,9 +85,10 @@ app.get('/file/:channelid/:fileid', (req, res) => {
 
     try {
         //ファイルを返す
-        res.sendFile(__dirname + "/files/" + channelid + "/" + fileidPathName + "/" + fileidIndex[fileid].name); //ユーザーデータのJSON読み込み);
+        console.log("返すファイルデータ :: ", fileidIndex[fileid]);
+        res.download(__dirname + "/files/" + channelid + "/" + fileidPathName + "/" + fileidIndex[fileid].name, fileidIndex[fileid].name); //ユーザーデータのJSON読み込み);
     } catch(e) {
-        console.log("index :: app.get('/file/') : ファイル送信失敗", e)
+        console.log("index :: app.get('/file/') : ファイル送信失敗", e);
         res.send("ファイルがねえ");
     }
 
