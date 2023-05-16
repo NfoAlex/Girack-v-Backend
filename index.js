e07c6b5054b4a7812cdc7b7a1e580b3e7bef78bc
バシュロアレキサンダーレイ <40430040+NfoAlex@users.noreply.github.com>
2023-05-16 11:05:14 +0900

    [change] - バージョン情報の更新

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const e = require("express");
 
 const port = process.env.PORT || 33333;
 
-const SERVER_VERSION = "alpha_20230515";
+const SERVER_VERSION = "alpha_20230516";
 
 const app = express();
 const server = http.createServer(app);
