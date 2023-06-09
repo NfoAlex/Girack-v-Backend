08f4aa019034885750628e85070c7ed7c0d1acd0
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2023-06-09 17:30:42 +0900

    [add] - バージョン情報の更新

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const e = require("express");
 
 const port = process.env.PORT || 33333;
 
-const SERVER_VERSION = "alpha_20230608";
+const SERVER_VERSION = "alpha_20230609";
 
 const app = express();
 const server = http.createServer(app);
