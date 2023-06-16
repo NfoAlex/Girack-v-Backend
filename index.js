81584a0ec82ada679d97b3a0f0f6f4fe39df9409
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2023-06-16 14:55:34 +0900

    Revert "[change] - バージョン情報の更新"
    This reverts commit 6973ddcbb456db1a05018871527df086d81d9a22.

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const e = require("express");
 
 const port = process.env.PORT || 33333;
 
-const SERVER_VERSION = "alpha_20230616";
+const SERVER_VERSION = "alpha_20230614";
 
 const app = express();
 const server = http.createServer(app);
