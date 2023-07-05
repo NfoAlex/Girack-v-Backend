d4c60c32969354ff26f999c21c975dd5cf0efdc2
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2023-07-05 13:00:45 +0900

    [add] - ログアウト処理にセッションデータを削除する部分を追加

--- index.js
+++ index.js
@@ -1056,6 +1056,9 @@ io.on("connection", (socket) => {
 
         }
 
+        //対象のセッションを削除
+        delete db.dataUser.user[dat.reqSender.userid].sessions[dat.reqSender.sessionid];
+
         //ユーザーのオンライン状態をオフラインとして設定
         db.dataUser.user[dat.reqSender.userid].state.loggedin = false;
         //DBをJSONへ保存
