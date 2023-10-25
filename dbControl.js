f7b6c00f9311e827574f5a7a2ec98a0a284f2410
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2023-10-25 11:28:01 +0900

    [change] - 初回ユーザーのasync化テスト

--- dbControl.js
+++ dbControl.js
@@ -1,54 +1,60 @@
 const fs = require('fs');
+const bcrypt = require("bcrypt"); //ハッシュ化用
 
 //ユーザーを記録しているJSONファイルを読み取る
 let dataUser = {};
 try { //読み込んでみる
     dataUser = JSON.parse(fs.readFileSync('./user.json', 'utf-8')); //ユーザーデータのJSON読み込み
 } catch(e) {
-    //最初のユーザー用のパスワードを生成
-    const pwLength = 24; //生成したい文字列の長さ
-    const pwSource = "abcdefghijklmnopqrstuvwxyz0123456789"; //元になる文字
-    let pwGenResult = "";
+    async () => {
+        //最初のユーザー用のパスワードを生成
+        const pwLength = 24; //生成したい文字列の長さ
+        const pwSource = "abcdefghijklmnopqrstuvwxyz0123456789"; //元になる文字
+        let pwGenResult = "";
 
-    //生成
-    for(let i=0; i<pwLength; i++){
-        pwGenResult += pwSource[Math.floor(Math.random() * pwSource.length)];
+        //生成
+        for(let i=0; i<pwLength; i++){
+            pwGenResult += pwSource[Math.floor(Math.random() * pwSource.length)];
 
-    }
-
-    //初期のユーザーデータ
-    let dataUserInitText = `
-{
-    "user":{
-        "00000001": {
-            "name": "Admin",
-            "role": "Admin",
-            "pw": "` + pwGenResult + `",
-            "state": {
-                "loggedin": false,
-                "session_id": "",
-                "banned": false
-            },
-            "channel": [
-                "0001"
-            ]
         }
-    }
-}`;
 
-    fs.writeFileSync("./user.json", dataUserInitText); //JSONファイルを作成
-    dataUser = JSON.parse(fs.readFileSync("./user.json", "utf-8")); //ユーザーデータのJSON読み込み
-    
-    //初回起動時にログインを促すためのメッセージ
-    console.log("***********************************");
-    console.log("***********************************");
-    console.log("Girackへようこそ!");
-    console.log("次のユーザー情報でログインしてください。");
-    console.log("\n");
-    console.log("パスワード : " + pwGenResult)
-    console.log("\n");
-    console.log("***********************************");
-    console.log("***********************************");
+        //パスワードをハッシュ化
+        let pwGenResultHashed = await bcrypt.hash(pwGenResult, 10);
+
+        //初期のユーザーデータ
+        let dataUserInitText = `
+        {
+            "user":{
+                "00000001": {
+                    "name": "Admin",
+                    "role": "Admin",
+                    "pw": "` + pwGenResultHashed + `",
+                    "state": {
+                        "loggedin": false,
+                        "session_id": "",
+                        "banned": false
+                    },
+                    "channel": [
+                        "0001"
+                    ]
+                }
+            }
+        }`;
+
+        fs.writeFileSync("./user.json", dataUserInitText); //JSONファイルを作成
+        dataUser = JSON.parse(fs.readFileSync("./user.json", "utf-8")); //ユーザーデータのJSON読み込み
+        
+        //初回起動時にログインを促すためのメッセージ
+        console.log("***********************************");
+        console.log("***********************************");
+        console.log("Girackへようこそ!");
+        console.log("次のユーザー情報でログインしてください。");
+        console.log("\n");
+        console.log("パスワード : " + pwGenResult)
+        console.log("\n");
+        console.log("***********************************");
+        console.log("***********************************");
+    }
 }
 
 //サーバー情報や設定を記録しているJSONファイルを読み取る
