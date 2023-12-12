e61d9220c27cf6299629522db278e2adc6440ad2
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2023-12-12 18:11:40 +0900

    [change] - バージョン情報の更新

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const socketIo = require("socket.io");
 
 const port = process.env.PORT || 33333;
 
-const SERVER_VERSION = "alpha_20231205";
+const SERVER_VERSION = "alpha_20231212";
 
 const app = express();
 const server = http.createServer(app);
