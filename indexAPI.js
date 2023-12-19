7273175928c0b9cc863f55e7af56457f865e791c
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2023-12-19 23:26:05 +0900

    [add] - サーバーを使うための導入

--- indexAPI.js
+++ indexAPI.js
@@ -1,1 +1,7 @@
-const apiMan = require("./apiControl.js"); //APIデータ関連
+const io = require('socket.io-client');
+
+//APIデータ関連
+const apiMan = require("./apiControl.js");
+
+//サーバー接続
+const socket = io('http://localhost:33333');
