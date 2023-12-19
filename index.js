995c4d5b4ac988bcb5df776f8af498a902b56bd4
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2023-12-19 12:01:29 +0900

    [add] - APIを有効化するsocketハンドラを作成

--- index.js
+++ index.js
@@ -1171,6 +1171,29 @@ io.on("connection", (socket) => {
 
     });
 
+    //APIを有効化（許可）
+    socket.on("activateApi", (dat) => {
+        /*
+        dat
+        {
+            apiId: 12345...,
+            reqSender: {...}
+        }
+        */
+
+        //セッション認証
+        if ( !checkDataIntegrality(dat, ["apiId"], "activateApi") ) return -1;
+
+        //有効化
+        apiMan.activateApi(dat);
+
+        //APIの情報取得
+        let dataAPI = apiMan.getApiList(dat.reqSender.userid);
+
+        //取得情報を送信
+        socket.emit("InfoApiList", dataAPI);
+
+    });
 
 // ===========================================================
 // 認証関連
