936128f259f2ea2abe642bfceeab5a71b29c0891
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2023-12-31 18:11:39 +0900

    [add] - interface定義用ファイル追加

---
+++ src/interfaceSrc.ts
@@ -0,0 +1,77 @@
+//サーバーデータ用の型定義
+export interface dataServer {
+    servername: string,
+    "registration": {
+        "available": boolean,
+        "invite": {
+            "inviteOnly": boolean,
+            "inviteCode": string
+        }
+    },
+    "config": {
+        "PROFILE": {
+            "PROFILE_ICON_MAXSIZE": string,
+            "PROFILE_USERNAME_MAXLENGTH": number
+        },
+        "CHANNEL": {
+            "CHANNEL_DEFAULT_REGISTERANNOUNCE": string,
+            "CHANNEL_DEFAULT_JOINONREGISTER": string[],
+            "CHANNEL_CREATE_AVAILABLE": boolean,
+            "CHANNEL_DELETE_AVAILABLEFORMEMBER": boolean,
+            "CHANNEL_PRIVATIZE_AVAILABLEFORMEMBER": boolean
+        },
+        "MESSAGE": {
+            "MESSAGE_PIN_ROLE": string,
+            "MESSAGE_TXT_MAXLENGTH": string,
+            "MESSAGE_FILE_MAXSIZE": string
+        }
+    },
+    "channels": {
+        [key:string]: {
+            name: string,
+            description: string,
+            pins: any[],
+            scope: string,
+            canTalk: string
+        }
+    }
+};
+
+export interface channel {
+    [key:string]: {
+        name: string,
+        description: string,
+        pins: any[],
+        scope: string,
+        canTalk: string
+    }
+}
+
+//ユーザーデータの型定義
+export interface dataUser {
+    user: {
+        [key: string]: {
+            name: string,
+            role: string,
+            pw: string,
+            state: {
+                loggedin: boolean,
+                sessions: {
+                    [key:string]: {
+                        sessionName: string,
+                        loggedinTime: string,
+                        loggedinTimeFirst: string
+                    }
+                },
+                banned: boolean
+            },
+            channel: string[]
+        }
+    }
+};
+
+//リクエストの送信者
+export interface reqSender {
+    userid: string,
+    sessionid: string
+};
