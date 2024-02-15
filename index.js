d5a73678cc8d66581cce13e2059301eaf7c3cec2
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2024-02-15 22:52:37 +0900

    [change] - バージョン情報の更新

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const socketIo = require("socket.io");
 const auth = require("./src/auth.js");
 
 //サーバーバージョン
-const SERVER_VERSION = "alpha_20240214";
+const SERVER_VERSION = "alpha_20240215";
 exports.SERVER_VERSION = SERVER_VERSION;
 
 //接続しているSocketJSON
